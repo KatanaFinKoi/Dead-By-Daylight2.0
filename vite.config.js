d38812670ab0575a09d1840c7a5b943ea30a6f97
KatanaFinKoi <liam.rayback@gmail.com>
2025-05-21 07:57:57 -0600

    updated vite config to hopefully fix deployment issues on gituhb pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: /Dead-By-Daylight2.0/,
   server: {
   open: true 
 }
