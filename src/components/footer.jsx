9ee7b6a20a5762578e8f6424da5196ee480c2c2d
KatanaFinKoi <liam.rayback@gmail.com>
2025-05-28 08:03:58 -0600

    added basic footer code. Need to implement footer later

---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+const footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto text-center">
+        <p className="footer-text">
+            This project is intended for educational and fan purposes only. Dead by Daylight and all related characters, storylines, and imagery are the property of Behaviour Interactive.
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default footer;
